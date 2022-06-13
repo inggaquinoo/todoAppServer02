41a537bbba56782a6e39ddbb6723ef1a41d126c2
Gustavo Aquino <inggaquinoo@gmail.com>
2022-06-12 23:02:16 -0500

    faith

--- server.js
+++ server.js
@@ -26,6 +26,6 @@ const PORT = process.env.PORT || 4000
 //
 
     app.listen(PORT, function() {
-        console.log("Server is running on port ->->-> " + PORT);
+        console.log("Server is running on port ->->->-> " + PORT);
     } )
 
